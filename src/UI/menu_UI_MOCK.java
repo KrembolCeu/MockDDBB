cdcd25b878418c370c01eb5c137a264061ff43b9
KrembolCeu <jorge.larreamosquera1@usp.ceu.es>
2025-03-14 10:20:02 +0100

    Intento nuevo repositorio para practicar

--- src/UI/menu_UI_MOCK.java
+++ src/UI/menu_UI_MOCK.java
@@ -4,7 +4,7 @@ public class menu_UI_MOCK {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.print("Hellow world");
+		System.out.print("Hello world 2");
 	}
 
 }
